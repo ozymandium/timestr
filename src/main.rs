2082aada60e8bde4cd3dd77f26db95b8d7e0a31b
ozymandium <dev@cofield.io>
2025-01-22 15:59:51 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use tabled::{settings::Style, Table, Tabled};
+
+#[derive(Tabled)]
+struct Results {
+    input: String,
+    temporis: String,
+    natural_date_parser: String,
+    chrono_english: String,
+    parse_datetime: String,
+    tu: String,
+}
+
+fn main() {
+    let inputs = vec![
+        "tomorrow",
+        "today",
+        "yesterday",
+        "1PM",
+        "1300",
+        "13:00",
+        "1:15",
+        "2024",
+        "March",
+        "last March",
+        "next March",
+        "March 2024",
+        "2024 March",
+        "March 10",
+        "a year ago",
+        "1 year ago",
+        "1 year",
+        "this week",
+        "last week",
+        "next week",
+        "1PM Pacific Time",
+        "1PM Eastern Time",
+        "1PM EDT",
+        "1PM EST",
+    ];
+    let mut results = vec![];
+    let red_x: String = "❌".to_string();
+    for input in inputs {
+        let mut result = Results {
+            input: input.to_string(),
+            temporis: "".to_string(),
+            natural_date_parser: "".to_string(),
+            chrono_english: "".to_string(),
+            parse_datetime: "".to_string(),
+            tu: "".to_string(),
+        };
+        // temporis
+        match temporis::parse_date(&input) {
+            Ok(date) => {
+                result.temporis = date.to_string();
+            }
+            Err(_) => {
+                result.temporis = red_x.clone();
+            }
+        }
+        // natural_date_parser
+        match natural_date_parser::date_parser::from_string(&input) {
+            Ok(date) => {
+                result.natural_date_parser = date.to_string();
+            }
+            Err(_) => {
+                result.natural_date_parser = red_x.clone();
+            }
+        }
+        // chrono-english
+        match chrono_english::parse_date_string(
+            &input,
+            chrono::Local::now(),
+            chrono_english::Dialect::Us,
+        ) {
+            Ok(date) => {
+                result.chrono_english = date.to_string();
+            }
+            Err(_) => {
+                result.chrono_english = red_x.clone();
+            }
+        }
+        // parse_datetime
+        match parse_datetime::parse_datetime(&input) {
+            Ok(date) => {
+                result.parse_datetime = date.to_string();
+            }
+            Err(_) => {
+                result.parse_datetime = red_x.clone();
+            }
+        }
+        // tu
+        match tu::parse_date_args(
+            &input
+                .split_whitespace()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>(),
+            chrono::Local::now().into(),
+        ) {
+            Ok(date) => {
+                result.tu = date.to_string();
+            }
+            Err(_) => {
+                result.tu = red_x.clone();
+            }
+        }
+        results.push(result);
+    }
+    let mut table = Table::new(&results);
+    table.with(Style::sharp());
+    println!("{}", table);
+}
